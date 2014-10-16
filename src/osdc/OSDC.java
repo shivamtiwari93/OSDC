232ef4e9fc8f56850ac8cafc1bdfdae55b70b0c3
shivamtiwari93 <shivamtiwari1011@gmail.com>
2014-10-16 17:36:38 +0530

    OSDC Ver 2.0

---
+++ src/osdc/OSDC.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package osdc;
+
+/**
+ *
+ * @author Shivam Tiwari
+ */
+public class OSDC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("OSDC");
+        System.out.println("JIIT");
+        
+    }
+    
+}
